a067b415dc15f7ef3297562240ac19ca5742a0ac
nicoosk <nicolas.hui18@gmail.com>
2024-04-16 23:41:36 -0400

    Subiendo rama

---
+++ basico/elevar.py
@@ -0,0 +1,6 @@
+import math
+
+n = 0.44 # base
+m = 4 # potencia
+
+print(f"El resultado de {n} ^ {m} es: {math.pow(n, m)}")
---
+++ basico/factorial.py
@@ -0,0 +1,6 @@
+import math
+
+# Parámetro
+n = 4 # Numero para calcularle el factorial
+
+print(f"El factorial de {n} es: {math.factorial(n)}")
